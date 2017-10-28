8b7bec4e3fe65b5923de93fb6603ea3b45fb2846
yakub miastoff <iakub.urban@gmail.com>
2017-10-28 23:57:18 +0200

    Implemented Advertisement Model and created Context class for it

--- AddStop/AddSpot-API/Models/Advertisement.cs
+++ AddStop/AddSpot-API/Models/Advertisement.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AddSpot_API.Enums;
+
 namespace AddSpot_API.Models
 {
     public class Advertisement
     {
-        public Advertisement()
-        {
-        }
+        public long Id { get; set; }
+        public User OwnerId { get; set; }
+        public User MateId { get; set; }
+        public decimal DestLat { get; set; }
+        public decimal DestLong { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime TripDate { get; set; }
+
+        [Range(18, int.MaxValue)]
+        public int RequiredAge { get; set; }
+        public Sex RequiredSex { get; set; }
+
+        public bool OwnerAccepted { get; set; }
+        public bool MateAccepted { get; set; }
+
+        public AdvertState State { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateAdded { get; set; }
     }
 }
--- AddStop/AddSpot-API/Models/AdvertisementContext.cs
+++ AddStop/AddSpot-API/Models/AdvertisementContext.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 namespace AddSpot_API.Models
 {
-    public class AdvertisementContext
+    public class AdvertisementContext : DbContext
     {
         public AdvertisementContext()
         {
