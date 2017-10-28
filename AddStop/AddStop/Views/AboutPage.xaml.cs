7d773396bf0a1f392cc3e386f3cd62dd56657dee
yakub miastoff <iakub.urban@gmail.com>
2017-10-28 15:13:32 +0200

    first commit - created all the projects and .gitignore

---
+++ AddStop/AddStop/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace AddStop
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
