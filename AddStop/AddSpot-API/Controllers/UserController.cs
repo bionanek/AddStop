d030276152bc20c4f468876baff5d8d9d50fa6b6
yakub miastoff <iakub.urban@gmail.com>
2017-10-28 19:54:07 +0200

    implemeneted GetAll and GetById methods for UserController

--- AddStop/AddSpot-API/Controllers/UserController.cs
+++ AddStop/AddSpot-API/Controllers/UserController.cs
@@ -2,43 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AddSpot_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AddSpot_API.Controllers
 {
+    
     [Route("api/[controller]")]
     public class UserController : Controller
     {
-        // GET: api/values
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if(_context.Users.Count() <= 0)
+            {
+                _context.Users.Add(new User {
+                    Login = "user",
+                    Password = "user",
+                    Name = "Janusz",
+                    Surname = "Ananas"
+                });
+                _context.SaveChanges();
+            }
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Users.ToList();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetUser")]
+        public IActionResult GetById(long id)
         {
-            return "value";
+            var item = _context.Users.FirstOrDefault(t => t.Id == id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+        //// GET: api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
