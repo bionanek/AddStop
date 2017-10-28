eb9c94b9d981d202765846cb9b44908161e9d64e
yakub miastoff <iakub.urban@gmail.com>
2017-10-29 00:07:22 +0200

    added lat and long of position from where the advert was added

--- AddStop/AddSpot-API/Models/Advertisement.cs
+++ AddStop/AddSpot-API/Models/Advertisement.cs
@@ -12,6 +12,9 @@ namespace AddSpot_API.Models
         public decimal DestLat { get; set; }
         public decimal DestLong { get; set; }
 
+        public decimal AddedFromLat { get; set; }
+        public decimal AddedFromLong { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime TripDate { get; set; }
